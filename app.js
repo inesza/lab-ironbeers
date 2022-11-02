3a470094f7d9de1e67a57157f49a9168aba7f9a4
Ines Zallouz <ines.zallouz@gmail.com>
2022-11-02 17:48:38 +0100

    Completed lab

--- app.js
+++ app.js
@@ -12,14 +12,30 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: "Home", class: "page-home" });
 });
+
+app.get('/beers', async (req, res) => {
+  const allBeers = await punkAPI.getBeers()
+  res.render('beers', { title: "All beers", class: "page-beers", allBeers: allBeers })
+});
+
+app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom()
+  res.render('single-beer', { title: "Random beer", class: "page-single-beer", beer: randomBeer })
+})
+
+app.get("/beer/:id", async (req, res) => {
+  const beer = await punkAPI.getBeer(req.params.id)
+  res.render('single-beer', { title: beer[0].name, class: "page-single-beer", beer: beer })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
